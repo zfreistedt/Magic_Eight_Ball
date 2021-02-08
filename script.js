c1806cc71921d518266e801f8e864b22872b3815
zfreistedt <zfreistedt@gmail.com>
2021-02-08 15:42:07 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function RunEightBall() {
+
+
+  let inputbox = document.getElementById("inputbox");
+  let eightBall = "";
+  let answer = document.getElementById("answer")
+  const randomNumber = Math.floor(Math.random() * 8);
+
+  switch (randomNumber) {
+    case 0:
+      eightBall = 'It is certain';
+      break;
+    case 1:
+      eightBall = 'It is decidedly so';
+      break;
+    case 2:
+      eightBall = 'Reply hazy try again';
+      break;
+    case 3:
+      eightBall = 'Cannot predict now';
+      break;
+    case 4:
+      eightBall = 'Do not count on it';
+      break;
+    case 5:
+      eightBall = 'My sources say no';
+      break;
+    case 6:
+      eightBall = 'Outlook not so good';
+      break;
+    case 7:
+      eightBall = 'Signs point to yes';
+      break;
+    default:
+      eightBall = 'Error';
+      break;
+  }
+
+  answer.innerHTML = eightBall;
+  inputbox.value = '';
+
+};
